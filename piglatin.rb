820b71a42523d10242ef74997af39809b83bb379
Jin <hojin.yoo@outlook.com>
2013-06-04 15:24:06 -0400

    Yeah

--- piglatin.rb
+++ piglatin.rb
@@ -1,7 +1,17 @@
- def piglatin(x)
- 	x.downcase
- 	puts x
+def piglatin(x)
+ 	array = x.downcase.split("")
+ 	until "aeiou".include? array.first
+ 		first_letter = array.shift
+ 		array = array + first_letter.to_a
+ 	end
 
- end
+ 	array = array + ["ay"]
+	return array.join
+end
 
- piglatin("tREst")
+puts piglatin("pig")
+puts piglatin("computer")
+puts piglatin("freedom")
+puts piglatin("string")
+puts piglatin("String")
+puts piglatin("StrinG")
