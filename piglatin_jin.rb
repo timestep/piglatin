cf619ec120eb3c4de6676e2d87e133e3a9d04d9b
Jin <hojin.yoo@outlook.com>
2013-06-10 13:14:19 -0400

    partially done

--- piglatin_jin.rb
+++ piglatin_jin.rb
@@ -1,20 +1,16 @@
 def piglatin(x)
  	array = x.downcase.split("")
+
  	until "aeiou".include? array.first
  		first_letter = array.shift
- 		array = array + first_letter.to_a
+ 		array = array + [first_letter]
  	end
 
  	array = array + ["ay"]
 	return array.join
+
 end
 
 
-puts piglatin("pig")
-puts piglatin("computer")
-puts piglatin("freedom")
-puts piglatin("string")
-puts piglatin("String")
-puts piglatin("StrinG")
-
-puts piglatin("pig").class
+puts piglatin("tree")
+puts piglatin(tree)
