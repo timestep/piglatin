ae19ac922757b0fa8daa3242c96153143c560231
timestep <aaronksivbalan@gmail.com>
2013-06-10 12:25:52 -0400

    addstuff

--- piglatinkevin.rb
+++ piglatinkevin.rb
@@ -1,9 +1,8 @@
 def piglatin
-	x = gets.chomp 
- 	i = x.index(/[aeiou]/i)
+	x = gets.chomp
+	i = x.index(/[aeiou]/i)
  	x = x.downcase.split("")
  	1.upto(i) { x<<x.shift }  		
  	puts x.join + "ay"
 end
 
-piglatin
